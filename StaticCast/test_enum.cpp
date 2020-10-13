37b51e514468495df9e5410b4743a690f372a34a
banchev2001 <banchev2001@abv.bg>
2020-10-13 23:26:31 +0300

    static_cast example

---
+++ StaticCast/test_enum.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+This example show how unary operators 'static_cast' work the links below: 
+https://en.cppreference.com/w/cpp/language/static_cast
+https://www.geeksforgeeks.org/static_cast-in-c-type-casting-operators/
+*/
+
+/*
+VERY IMPORTANT!!!
+*/
+
+/*
+Add More Comment!!
+*/
+
+int RndDig, cnt;
+int ind;
+
+enum  MyCondition  { Con1, Con2, Con3} Condition;
+
+int main(){
+  
+    for(int i=0; i < 5; i++ ){
+        printf("Condition %d\n",Condition +1);
+        //Calculate next enum                  Calculation    This part return to first member
+        Condition = static_cast<MyCondition>((Condition+1) % (Con3 + 1));   
+    }
+    return 0;
+}
