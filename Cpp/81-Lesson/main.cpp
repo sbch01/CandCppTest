2bfebac18d6c05aa44d649ec9ac3c42bf9f53591
sbch01 <banchev2001@abv.bg>
2021-09-30 21:40:41 +0300

    new pointers lessons

--- Cpp/81-Lesson/main.cpp
+++ Cpp/81-Lesson/main.cpp
@@ -19,12 +19,28 @@ using namespace std;
 int main(){
 
 int a = 11,b=34,c=88;
-int* ptr_int ;//= NULL;
+char symbol;
+float Value;
+
+//void указател е като универсале, може да приема адреси на всякакъв тип променливи
+void * ptr;
+
+//указател който може да съдържа адрес на променлива от тип int
+int* ptr_int = NULL;
 
 cout<<"Addres of a: "<<&a<<endl
-<<"Addres of b: "<<&b<<endl
-<<"Addres of c: "<<&c<<endl;
+    <<"Addres of b: "<<&b<<endl
+    <<"Addres of c: "<<&c<<endl;
 
+ptr = &symbol;
+cout<<"First void pointer value: "<<ptr<<endl;
+ptr = &Value;
+cout<<"Second void pointer value: "<<ptr<<endl;
+ptr = &a;
+cout<<"Third void pointer value: "<<ptr<<endl;
+
+
+//стойност на нулев пойнтер
 cout<<endl<<"Null int pointer contain: "<<ptr_int<<endl;
 
 }
