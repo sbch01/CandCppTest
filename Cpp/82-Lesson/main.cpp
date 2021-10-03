ded66e222b9c51f645c2321332c87220f65120a6
sbch01 <banchev2001@abv.bg>
2021-10-03 08:35:11 +0300

    new pointer lessons

--- Cpp/82-Lesson/main.cpp
+++ Cpp/82-Lesson/main.cpp
@@ -15,9 +15,10 @@ int main(){
 
     //декларираме стринг като масив
     char Mystring1[]= "-This is pointers test";
+    
     //декларираме стринг като указател към първия елемент от стринга, нужно е да се ползва const
-    char const * Mystring2 = "-This is another pointer test"; //пробвай без const, компилатора дава грешка
-
+    const char* Mystring2 = "-This is another pointer test"; //пробвай без const, компилатора дава грешка
+    
 
 
     //бенефициента е че можем да отместваме указателя и така да показваме желана част от стринга
